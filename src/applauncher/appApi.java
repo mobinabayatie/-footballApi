9985ef83dc5e9918ca2a200b13a6537c076a1636
mobinabayatie <mobinabayatie@gmail.com>
2024-01-25 22:24:20 +0330

    second commit

--- src/applauncher/appApi.java
+++ src/applauncher/appApi.java
@@ -1,5 +1,43 @@
 package applauncher;
 
-public class appApi {
+import org.json.simple.JSONArray;
 
+public class appApi {
+	 public static JSONArray getLeagueData(int LeagueID){
+	        
+	        
+	        // set Season value
+	        int seasonYear;
+	        
+	        // if league id is for world cup
+	        if(LeagueID == 1){
+	            seasonYear = 2022;
+	        }
+	        else{
+	            seasonYear = 2023;
+	        }
+	        
+	              
+	        // build API request URL with LeagueID
+		String urlString = "https://v3.football.api-sports.io/fixtures?" +
+		        "league=" + LeagueID + "&season=" + seasonYear + "&status=FT";
+	        
+	        try{
+	            // call api and get response
+		    HttpURLConnection conn = fetchApiResponse(urlString);
+	            
+	            // check for response status
+		    // 200 - means that the connection was a success
+		    if(conn.getResponseCode() != 200){
+		        System.out.println("Error: Could not connect to API");
+		        return null;
+		    }
+	            
+	            // store resulting json data
+	            StringBuilder resultJson = new StringBuilder();
+	            Scanner scanner = new Scanner(conn.getInputStream());
+		    while(scanner.hasNext()){
+		        // read and store into the string builder
+		        resultJson.append(scanner.nextLine());
+		    }
 }
--- src/applauncher/footballApp.java
+++ src/applauncher/footballApp.java
@@ -1,5 +1,79 @@
 package applauncher;
 
-public class footballApp {
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class footballApp extends JFrame {
+	 private JSONArray leagueData;
+	  private static JPanel Panel;
+	    
+	    public footballApp() {
+	        
+		// Setup GUI and Add a Title
+		super("برنامه نتایج فوتبال");
+			
+		// Config Gui end program it has been closed
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
+		// Set the Size of Our GUI (in Pixels)
+		setSize(1080,650);
+			
+		// Load Our Application at the Center of Screen
+		setLocationRelativeTo(null);
+			
+	        // set manually position Our Components
+	        setLayout(null);
+			
+		// Prevent any Resize
+		setResizable(false);
+	        
+	        // Set background color
+	        Color backgroundColor = new Color(86, 183, 254);
+	        getContentPane().setBackground(backgroundColor);
+			
+		addGuiComponents();
+	    }
+	    private void addGuiComponents(){
+	        
+	        
+	        // set colors for buttons
+	        Color ButtonColor = new Color(236, 198, 1);
+	        
+	        // set fonts for buttons
+	        Font buttonFont = new Font("Arial", Font.BOLD, 16);
+	        
+	        
+	        // italy Button
+	        JButton Button1 = new JButton("Italy");
+	        Button1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		Button1.setBounds(890, 30, 120, 50);
+	        Button1.setBackground(ButtonColor);
+	        Button1.setFont(buttonFont);
+	        Button1.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData = appApi.getLeagueData(135);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }           
+	            }          
+	        });
+	        add(Button1);
+	        
+
+	    
 }
