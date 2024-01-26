35d08c5765bb1fb7a1daf9f1c4ba37ad141caf67
mobinabayatie <mobinabayatie@gmail.com>
2024-01-26 18:22:48 +0330

    final uplouding

--- src/applauncher/appLauncher.java
+++ src/applauncher/appLauncher.java
@@ -1,5 +1,20 @@
 package applauncher;
 
+import javax.swing.SwingUtilities;
+
 public class appLauncher {
+
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+		new footballApp().setVisible(true);
+
+            }
+        });
+        
+        
+    }
 
 }
--- src/applauncher/footballApp.java
+++ src/applauncher/footballApp.java
@@ -2,6 +2,7 @@ package applauncher;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -9,6 +10,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -231,6 +233,82 @@ public class footballApp extends JFrame {
 	        scrollPane.setBounds(50, 100, 960, 450);
 	        getContentPane().add(scrollPane);
 	    }
-	    
+	    private void showFixtureDetails(JSONObject fixtureObject) {
+	        
+	        // get fixture,league,team,goals from fixtureObject
+	        JSONObject fixture = (JSONObject) fixtureObject.get("fixture");
+	        JSONObject league = (JSONObject) fixtureObject.get("league"); 
+	        JSONObject teams = (JSONObject) fixtureObject.get("teams"); 
+	        JSONObject goals = (JSONObject) fixtureObject.get("goals");
+	        
+	        // get home and away from teams
+	        JSONObject homeTeam = (JSONObject) teams.get("home");
+	        JSONObject awayTeam = (JSONObject) teams.get("away");
+	        
+	        // get venue from fixture
+	        JSONObject venue = (JSONObject) fixture.get("venue");
+	        
+	        // set color for green panels
+	        Color rowColor = new Color(166, 210, 1);
+	        
+	        // set green panels
+	        JPanel rowPanel = new JPanel();
+	        rowPanel.setPreferredSize(new Dimension(400, 200));
+	        rowPanel.setBackground(rowColor);
+	        rowPanel.setLayout(null);
+	        Panel.add(rowPanel);
+	         
+	        // label for date
+	        JLabel label1 = new JLabel((String)fixture.get("date"));
+	        label1.setBounds(380, 30, 300, 50);
+	        label1.setFont(new Font("Arial", Font.PLAIN, 16));
+	        rowPanel.add(label1);
+	        
+	        // label for home name
+	        JLabel label2 = new JLabel((String)homeTeam.get("name"));
+	        label2.setBounds(150, 80, 250, 50);
+	        label2.setFont(new Font("Arial", Font.PLAIN, 18));
+	        rowPanel.add(label2);
+	        
+	        // label for home goals
+	        JLabel label3 = new JLabel(String.valueOf((Long)goals.get("home")));
+	        label3.setBounds(350, 80, 30, 50);
+	        label3.setFont(new Font("Arial", Font.BOLD, 25));
+	        rowPanel.add(label3);
+	        
+	        // label for away goals
+	        JLabel label4 = new JLabel(String.valueOf((Long)goals.get("away")));
+	        label4.setBounds(600, 80, 30, 50);
+	        label4.setFont(new Font("Arial", Font.BOLD, 25));
+	        rowPanel.add(label4); 
+	        
+	        // label for home name
+	        JLabel label5 = new JLabel((String)awayTeam.get("name"));
+	        label5.setBounds(700, 80, 180, 50);
+	        label5.setFont(new Font("Arial", Font.PLAIN, 18));
+	        rowPanel.add(label5);
+	        
+	        // label for round
+	        JLabel label6 = new JLabel((String)league.get("round"));
+	        label6.setBounds(420, 1, 200, 50);
+	        label6.setFont(new Font("Arial", Font.PLAIN, 16));
+	        rowPanel.add(label6);
+	        
+	        // label for referee
+	        JLabel label7 = new JLabel((String)fixture.get("referee"));
+	        label7.setBounds(400, 120, 500, 50);
+	        label7.setFont(new Font("Arial", Font.PLAIN, 16));
+	        rowPanel.add(label7);
+	        
+	        // label for referee
+	        JLabel label8 = new JLabel((String)venue.get("name") + " - " + venue.get("city") );
+	        label8.setBounds(400, 150, 500, 50);
+	        label8.setFont(new Font("Arial", Font.PLAIN, 16));
+	        rowPanel.add(label8);
+	        // reset main panel
+	        Panel.revalidate();
+	        Panel.repaint();
+	        
+	    }
 
 }
