c975c0f09e5124d06af8b04908671789698efb69
mobinabayatie <mobinabayatie@gmail.com>
2024-01-26 12:15:32 +0330

    uplouding changes

--- src/applauncher/appApi.java
+++ src/applauncher/appApi.java
@@ -1,6 +1,12 @@
 package applauncher;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class appApi {
 	 public static JSONArray getLeagueData(int LeagueID){
@@ -40,4 +46,52 @@ public class appApi {
 		        // read and store into the string builder
 		        resultJson.append(scanner.nextLine());
 		    }
+		    // close scanner
+		    scanner.close();
+
+		    // close url connection
+		    conn.disconnect();
+	            
+	            // parse through our data
+	            JSONParser parser = new JSONParser();
+		    JSONObject resultJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+	            
+	            // retrieve response data
+	            JSONArray responseArray = (JSONArray) resultJsonObj.get("response");
+	            
+	            return responseArray ;
+	            
+	        }catch(Exception e){
+		    e.printStackTrace();
+		}
+	        
+	        return null;
+	    }
+	    
+	 private static HttpURLConnection fetchApiResponse(String urlString){
+			try{
+			    // attempt to create connection
+			    URL url = new URL(urlString);
+			    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+			    // set request method to get
+			    conn.setRequestMethod("GET");
+		            
+		            // set request headers and api key
+		            conn.setRequestProperty("x-rapidapi-key", "ca4384d2759d61fbce7bf5f8d9786dca");
+		            conn.setRequestProperty("x-rapidapi-host", "v3.football.api-sports.io");
+		            
+
+			    // connect to our API
+			    conn.connect();
+			    return conn;
+		            
+			}catch(IOException e){
+			    e.printStackTrace();
+			}
+
+			// could not make connection
+			return null;
+		    }
+
 }
--- src/applauncher/footballApp.java
+++ src/applauncher/footballApp.java
@@ -3,10 +3,14 @@ package applauncher;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -45,7 +49,7 @@ public class footballApp extends JFrame {
 	        
 	        
 	        // set colors for buttons
-	        Color ButtonColor = new Color(236, 198, 1);
+	        Color ButtonColor = new Color(100, 100, 0);
 	        
 	        // set fonts for buttons
 	        Font buttonFont = new Font("Arial", Font.BOLD, 16);
@@ -54,7 +58,7 @@ public class footballApp extends JFrame {
 	        // italy Button
 	        JButton Button1 = new JButton("Italy");
 	        Button1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
-		Button1.setBounds(890, 30, 120, 50);
+		    Button1.setBounds(890, 30, 120, 50);
 	        Button1.setBackground(ButtonColor);
 	        Button1.setFont(buttonFont);
 	        Button1.addActionListener(new ActionListener() {
@@ -74,6 +78,159 @@ public class footballApp extends JFrame {
 	        });
 	        add(Button1);
 	        
+	        // france Button
+	        JButton Button2 = new JButton("France");
+	        Button2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button2.setBounds(330, 30, 120, 50);
+	        Button2.setBackground(ButtonColor);
+	        Button2.setFont(buttonFont);
+	        Button2.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData = appApi.getLeagueData(61);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }            
+	            }          
+	        });
+	        add(Button2);
 
+	        // iran Button
+	        JButton Button3 = new JButton("Iran"); 
+	        Button3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button3.setBounds(610, 30, 120, 50);
+	        Button3.setBackground(ButtonColor);
+	        Button3.setFont(buttonFont);
+	        Button3.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData =appApi .getLeagueData(290);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }            
+	            }          
+	        });
+	        add(Button3);
+ 
+	        // england Button
+	        JButton Button4 = new JButton("England");
+	        Button4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button4.setBounds(190, 30, 120, 50);
+	        Button4.setBackground(ButtonColor);
+	        Button4.setFont(buttonFont);
+	        Button4.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData = appApi.getLeagueData(39);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject); 
+	                }            
+	            }          
+	        });
+	        add(Button4);
+	        
+	        // spain Button
+	        JButton Button5 = new JButton("Spain");
+	        Button5.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button5.setBounds(470, 30, 120, 50);
+	        Button5.setBackground(ButtonColor);
+	        Button5.setFont(buttonFont);
+	        Button5.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData =appApi.getLeagueData(140);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }            
+	            }          
+	        });
+	        add(Button5);
+	        
+	        // germany Button
+	        JButton Button6 = new JButton("Germany");       
+	        Button6.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button6.setBounds(750, 30, 120, 50);
+	        Button6.setBackground(ButtonColor);
+	        Button6.setFont(buttonFont);
+	        Button6.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData = appApi.getLeagueData(78);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }            
+	            }          
+	        });
+	        add(Button6);
+	        
+	        // world cup Button
+	        JButton Button7 = new JButton("World Cup");
+	        Button7.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));			
+		    Button7.setBounds(50, 30, 120, 50);
+	        Button7.setBackground(ButtonColor);
+	        Button7.setFont(buttonFont);
+	        Button7.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                
+	                // remove all green panels form main panel
+	                Panel.removeAll();
+	                
+	                leagueData = appApi.getLeagueData(1);
+	                
+	                for (Object element : leagueData) {
+	                    JSONObject fixtureObject = (JSONObject) element;
+	                    showFixtureDetails(fixtureObject);
+	                }            
+	            }          
+	        });
+	        add(Button7);  
+	        
+	        // set panel color
+	        Color panelColor = new Color(100, 00, 00);
+	        
+	        // main panel setting
+	        Panel = new JPanel();
+	        Panel.setBounds(50, 100, 960, 450);
+	        Panel.setBackground(panelColor);
+	        Panel.setLayout(new GridLayout(0, 1,10,10));
+	        add(Panel);
+	        
+	        // add scroll to main panel
+	        JScrollPane scrollPane = new JScrollPane(Panel);
+	        scrollPane.setBounds(50, 100, 960, 450);
+	        getContentPane().add(scrollPane);
+	    }
 	    
+
 }
